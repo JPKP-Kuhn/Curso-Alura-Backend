194148c73f7ea23920e058a5f86ef6e8cc0758c4
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2024-09-14 00:00:18 -0300

    First project, screen match for movies

---
+++ first-project/src/Perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    public static void main(String[] args) {
+        System.out.println("Olá João Pedro!");
+        //Little operations:
+        System.out.println(10 + 5);
+        System.out.println(10 - 5);
+    }
+}
