e1172fcd5a31d0b3f73ec9c37cb70a455f6fc6e0
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2024-09-15 23:44:42 -0300

    string concatenation

--- Screen-Match/src/Main.java
+++ Screen-Match/src/Main.java
@@ -6,11 +6,25 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Esse é o Screen Match!");
         System.out.println("Filme: O Senhor dos Anéis: A sociedade do Anel");
+        String nomeFilme = "O Senhor dos Anéis: A sociedade do Anel";
 
         int anoDeLancamento = 2001;
         System.out.println("Ano de lançamento: " + anoDeLancamento);
 
         boolean incluidoNoPlano = true; //ou false
         double notaDoFilme = 9.9;
+
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                O filme '%s' é de fantasia fantástica
+                Foi muito aclamado pelo público, recebeu prêmio de melhores efeitos
+                Um grupo de aventureiros precisa destruir um anel.
+                """.formatted(nomeFilme);
+        System.out.println(sinopse);
+
+        int classificacao = (int) media / 2;
+        System.out.println(classificacao);
     }
 }
